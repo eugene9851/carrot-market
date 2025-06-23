668ed4cc3f86dc2273541166cc5453effd468ff6
eugene9851 <eugene9851@naver.com>
2025-06-23 17:59:35 +0900

    feature: apply zod in create-account

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,19 +1,25 @@
-import FormInput from "@/app/create-account/components/form-input";
-import FormButton from "@/app/create-account/components/form-button";
-import SocialLogin from "./components/social-login";
+'use client'
+
+import FormInput from "@/components/form-input";
+import FormButton from "@/components/form-button";
+import SocialLogin from "../../components/social-login";
+import { createAccount } from "./actions";
+import { useActionState } from "react";
 
 export default function CreateAccount() {
+  const [state, action] = useActionState(createAccount, null)
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput name="username" type="text" placeholder="Username" required errors={[]} />
-        <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
-        <FormInput name="password" type="password" placeholder="Password" required errors={[]} />
-        <FormInput name="passwordConfirm" type="password" placeholder="Confirm Password" required errors={[]} />
+      <form action={action} className="flex flex-col gap-3">
+        <FormInput name="username" type="text" placeholder="Username" required errors={state?.fieldErrors.username} />
+        <FormInput name="email" type="email" placeholder="Email" required errors={state?.fieldErrors.email} />
+        <FormInput name="password" type="password" placeholder="Password" required errors={state?.fieldErrors.password} />
+        <FormInput name="passwordConfirm" type="password" placeholder="Confirm Password" required errors={state?.fieldErrors.passwordConfirm} />
         <FormButton text="Create Account" />
       </form>
       <SocialLogin />

dd09051a6c96eb4579cdb8d2fe8de09385b103c9
eugene9851 <eugene9851@naver.com>
2025-06-23 16:18:39 +0900

    feature: add name in input for server actions, and handleForm

--- app/create-account/components/form-input.tsx
+++ app/create-account/components/form-input.tsx
@@ -3,14 +3,15 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
 export default function FormInput({
-  type, placeholder, required, errors
+  type, placeholder, required, errors, name
 }: FormInputProps) {
   return(
     <div className="flex flex-col gap-2">
-      <input className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400" type={type} placeholder={placeholder} required={required} />
+      <input name={name} className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400" type={type} placeholder={placeholder} required={required} />
       {errors.map((error, idx) => (
         <span key={idx} className="text-red-500 font-medium">{error}</span>
       ))}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,13 @@
-'use client'
-
 import FormInput from "@/app/create-account/components/form-input";
 import FormButton from "@/app/create-account/components/form-button";
 import SocialLogin from "@/app/create-account/components/social-login";
 
 export default function Login() {
-  const onClick = async () => {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: 'nico',
-        password: '1234',
-      })
-    })
-    console.log(await response.json())
+  const handleForm = async (formData: FormData) => {
+    'use server';
+
+    console.log(formData.get('email'), formData.get('password'))
+    console.log('i run in the server!')
   }
 
   return (
@@ -22,13 +16,11 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
-        <FormInput type="password" placeholder="Password" required errors={[]} />
-      </form>
-      <span onClick={onClick}>
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
+        <FormInput name="password" type="password" placeholder="Password" required errors={[]} />
         <FormButton text="Log in" loading={false} />
-      </span>
+      </form>
       <SocialLogin />
     </div>
   )

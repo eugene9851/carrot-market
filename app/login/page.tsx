f0b74b3e9f4ac210f6fc3e1e7372254b5805e931
eugene9851 <eugene9851@naver.com>
2025-06-23 16:01:42 +0900

    style: authentication UI

---
+++ app/login/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import FormInput from "@/app/create-account/components/form-input";
+import FormButton from "@/app/create-account/components/form-button";
+import SocialLogin from "@/app/create-account/components/social-login";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput type="password" placeholder="Password" required errors={[]} />
+      </form>
+      <FormButton text="Log in" loading={false} />
+      <SocialLogin />
+    </div>
+  )
+} 

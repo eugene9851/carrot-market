9f7a00fb743fddc56c32e58a29c1726a7d1058a3
eugene9851 <eugene9851@naver.com>
2025-07-07 16:05:26 +0900

    feature: relationships and onDelete

--- lib/db.ts
+++ lib/db.ts
@@ -3,22 +3,42 @@ import { PrismaClient } from "./generated/prisma";
 const db = new PrismaClient();
 
 async function test() {
-  const user = await db.user.create({
-    data: {
-      username: 'a;a;a;',
-    }
-  })
+  // const user = await db.user.create({
+  //   data: {
+  //     username: 'number2',
+  //   }
+  // })
+  // console.log(user)
 
-  const user2 = await db.user.findMany({
+  // const user2 = await db.user.findMany({
+  //   where: {
+  //     username: {
+  //       contains: 'est',
+  //     }
+  //   }
+  // })
+
+  // const token = await db.sMSToken.create({
+  //   data: {
+  //     token: '222222',
+  //     user: {
+  //       connect: {
+  //         id: 3,
+  //       }
+  //     }
+  //   }
+  // })
+
+  // console.log(token);
+
+  const token = await db.sMSToken.findUnique({
     where: {
-      username: {
-        contains: 'est',
-      }
+      id: 1
     }
   })
 
-  console.log(user);
-  console.log(user2);
+  console.log(token);
+
 }
 
 test();
